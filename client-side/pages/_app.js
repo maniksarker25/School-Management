8aa3bb7dbdf57a79c7b77e2753f4fc456c14efc0
mehedidev <mdmehedi23234@gmail.com>
2023-10-19 16:55:55 +0600

    layout

--- client-side/pages/_app.js
+++ client-side/pages/_app.js
@@ -1,10 +1,14 @@
+import Layout from "@/src/component/core/layout/Layout";
 import "@/styles/globals.css";
 import { ChakraProvider } from "@chakra-ui/react";
 
 export default function App({ Component, pageProps }) {
   return (
     <ChakraProvider>
+      <Layout>
       <Component {...pageProps} />
+
+      </Layout>
     </ChakraProvider>
   );
 }
