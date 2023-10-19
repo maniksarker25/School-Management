cb5255baf96a153b4bd0c018fb5256c9823d1bd9
Manik <tonysins43@gmail.com>
2023-10-19 16:16:32 +0600

    setup server side

---
+++ server-side/src/app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors")
+
+const app = express();
+
+app.use(express.json())
+app.use(cors());
+
+
+
+
+module.exports = app;
