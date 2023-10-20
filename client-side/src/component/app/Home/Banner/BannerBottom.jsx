8921dfa756de7a401347c6f42d1515c4a772a2fb
Manik <tonysins43@gmail.com>
2023-10-20 10:52:42 +0600

    add banner

---
+++ client-side/src/component/app/Home/Banner/BannerBottom.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import MailOutlineIcon from "@mui/icons-material/MailOutline";
+import HeadsetMicOutlinedIcon from '@mui/icons-material/HeadsetMicOutlined';
+import PersonAddAltOutlinedIcon from '@mui/icons-material/PersonAddAltOutlined';
+
+const BannerBottom = () => {
+  return (
+    <div style={{ backgroundColor: "#01A2A6" }}>
+      <div className="contain flex justify-between">
+        <div className="text-white py-12">
+          <MailOutlineIcon sx={{color:"white",fontSize:"45px"}}/>
+          <h4 className="mt-6 text-2xl mb-1.5" style={{color:"rgb(255,241,155)"}}>Get Quote</h4>
+          <p>Find the price of your course</p>
+        </div>
+        <div className="text-white py-12">
+          <HeadsetMicOutlinedIcon sx={{color:"white",fontSize:"45px"}}/>
+          <h4 className="mt-6 text-2xl mb-1.5" style={{color:"rgb(255,241,155)"}}>Ask a Question</h4>
+          <p>Not sure what you need? Ask our Advisor</p>
+        </div>
+        <div className="text-white py-12">
+          <PersonAddAltOutlinedIcon sx={{color:"white",fontSize:"45px"}}/>
+          <h4 className="mt-6 text-2xl mb-1.5" style={{color:"rgb(255,241,155)"}}>Join Today</h4>
+          <p>Join our community and learn</p>
+        </div>
+      
+      </div>
+    </div>
+  );
+};
+
+export default BannerBottom;
--- client-side/src/component/app/Home/Banner/index.jsx
+++ client-side/src/component/app/Home/Banner/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import BannerSlider from './BannerSlider';
+import BannerBottom from './BannerBottom';
 
 const Banner = () => {
     return (
         <div className='mt-4'>
           <BannerSlider/>
+          <BannerBottom/>
         </div>
     );
 };
