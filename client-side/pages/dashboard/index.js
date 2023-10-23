7533bf210633f65afc021672e92629ba39dd7039
toma570 <itextoma@gmail.com>
2023-10-23 12:41:55 +0600

    add event page

--- client-side/pages/dashboard/index.js
+++ client-side/pages/dashboard/index.js
@@ -1,11 +1,16 @@
 import DashboardLayout from '@/src/component/core/layout/DashboardLayout';
+import DashboardPageTitle from '@/src/component/share/Dashboard/DashboardPageTitle/DashboardPageTitle';
+import { Box } from '@chakra-ui/react';
 import React from 'react';
 
 const Dashboard = () => {
     return (
-		
+		<Box>
+			<Box>
+				<DashboardPageTitle title={"Dashboard"} />
+			</Box>
 			<p>this is dashboard mane file </p>
-		
+		</Box>
 	);
 };
 
