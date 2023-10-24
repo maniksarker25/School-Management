f45b512d3d9bfa73af9d837328bd4bb6175792bd
Manik <tonysins43@gmail.com>
2023-10-24 14:14:33 +0600

    s

--- client-side/src/component/app/Dashboard/Employee/AddEmployee/index.jsx
+++ client-side/src/component/app/Dashboard/Employee/AddEmployee/index.jsx
@@ -9,6 +9,11 @@ import Person2OutlinedIcon from "@mui/icons-material/Person2Outlined";
 import AddIcCallOutlinedIcon from '@mui/icons-material/AddIcCallOutlined';
 import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
 import BrokenImageOutlinedIcon from '@mui/icons-material/BrokenImageOutlined';
+import LockOpenIcon from '@mui/icons-material/LockOpen';
+import PublicIcon from '@mui/icons-material/Public';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import TwitterIcon from '@mui/icons-material/Twitter';
 const AddEmployee = () => {
   const {
     register,
@@ -261,7 +266,131 @@ const AddEmployee = () => {
                 <input type="file" name="profilePhoto" className="hidden" id="profilePhoto" />
               </div>
             </div>
-            <input type="submit" />
+            {/* login details */}
+            <div>
+            <div className="flex items-center mt-5 mb-3">
+                    <LockOpenIcon/>
+                    <p className="text-xl font-semibold">Login Details</p>
+                </div>
+                <div className="flex gap-4">
+                <div className="w-full">
+                <p className="mb-1">Username*</p>
+              <div>
+                 <div className="flex items-center ">
+                 <Person2OutlinedIcon className="border-2 text-[33px]" />
+                  <input type="text" name="" id="" placeholder="Username" className="nice-input"
+                  {...register("username", {
+                    required: true,
+                  })}
+                  aria-invalid={errors.username ? "true" : "false"}
+                  />
+                 </div>
+                  {errors.username?.type === "required" && (
+                      <Error error={"Username is required"} />
+                    )}
+                </div>
+              </div>
+                <div className="w-full">
+                <p className="mb-1">Password*</p>
+              <div>
+                 <div className="flex items-center ">
+                 <LockOpenIcon className="border-2 text-[33px]" />
+                  <input type="text" name="" id="" placeholder="Password" className="nice-input"
+                  {...register("password", {
+                    required: true,
+                    minLength: 6,
+                    maxLength: 20,
+                    pattern:
+                      /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z])/,
+                  })}
+                  />
+                    
+                 </div>
+                 {errors.password?.type === "required" && (
+                    <p className="text-red-600">Password is required</p>
+                  )}
+                  {errors.password?.type === "minLength" && (
+                    <p className="text-red-600">Password must be 6 character</p>
+                  )}
+                  {errors.password?.type === "maxLength" && (
+                    <p className="text-red-600">
+                      Password must be less then 20 character
+                    </p>
+                  )}
+                  {errors.password?.type === "pattern" && (
+                    <p className="text-red-600">
+                      Password must have one Uppercase one lower case, one
+                      number and one special character.
+                    </p>
+                  )}
+                </div>
+              </div>
+                </div>
+            </div>
+            {/* Social links */}
+            <div>
+            <div className="flex items-center mt-5 mb-3">
+                    <PublicIcon/>
+                    <p className="text-xl font-semibold">Social Links</p>
+                </div>
+
+                <div className="lg:flex gap-4 ">
+                <div className="w-full">
+                <p className="mb-1">Facebook</p>
+              <div>
+                 <div className="flex items-center ">
+                 <FacebookIcon className="border-2 text-[33px]" />
+                  <input type="text" name="" id="" placeholder="Username" className="nice-input"
+                  {...register("facebook", {
+                    required: true,
+                  })}
+                  aria-invalid={errors.facebook ? "true" : "false"}
+                  />
+                 </div>
+                  {errors.facebook?.type === "required" && (
+                      <Error error={"Facebook is required"} />
+                    )}
+                </div>
+              </div>
+                <div className="w-full">
+                <p className="mb-1">Linkedin</p>
+              <div>
+                 <div className="flex items-center ">
+                 <LinkedInIcon className="border-2 text-[33px]" />
+                  <input type="text" name="" id="" placeholder="Username" className="nice-input"
+                  {...register("linkedin", {
+                    required: true,
+                  })}
+                  aria-invalid={errors.linkedin ? "true" : "false"}
+                  />
+                 </div>
+                  {errors.linkedin?.type === "required" && (
+                      <Error error={"Linkedin is required"} />
+                    )}
+                </div>
+              </div>
+                <div className="w-full">
+                <p className="mb-1">Twitter</p>
+              <div>
+                 <div className="flex items-center ">
+                 <TwitterIcon className="border-2 text-[33px]" />
+                  <input type="text" name="" id="" placeholder="Username" className="nice-input"
+                  {...register("facebook", {
+                    required: true,
+                  })}
+                  aria-invalid={errors.twitter ? "true" : "false"}
+                  />
+                 </div>
+                  {errors.twitter?.type === "required" && (
+                      <Error error={"Twitter is required"} />
+                    )}
+                </div>
+              </div>
+                </div>
+            </div>
+            <div className="flex justify-end mt-6 ">
+            <input type="submit" value={"Save"} className="border-2 px-12 py-1.5 rounded-lg cursor-pointer" />
+            </div>
           </form>
         </div>
       </div>
